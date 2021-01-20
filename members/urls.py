723dcf6491aeb13a8ab7f028bff3c8c710b28371
root <imcjw93@gmail.com>
2021-01-20 15:50:42 +0900

    gugu

--- members/urls.py
+++ members/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index),
+    path('gugu',views.gu),
     path('test/',views.test),
     path('test2/',views.test2),
     path('signup/',views.signup)
--- members/views.py
+++ members/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from .models import Members
 # Create your views here.
+def gu(request) :
+    num =  request.GET.get('num','')
+    return HttpResponse(f'<h1>gugu: {num_gugu(num)}</h1>')
+
+def num_gugu(num):
+    str=""
+    for i in range(9):
+        str+=f"{int(num)}*{i+1}={int(num)*(i+1)}<br>"
+    return str
+
 def index(request):
     return HttpResponse("version1")
 
